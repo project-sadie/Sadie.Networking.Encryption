5212caf684add099775b451355db1474f0b7c798
habtard <habtard@gmail.com>
2025-04-23 18:57:09 +0100

    Initial commit

---
+++ Sadie.Networking.Encryption/EncryptionServiceCollection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sadie.Options.Options;
+
+namespace Sadie.Networking.Encryption;
+
+public static class EncryptionServiceCollection
+{
+    public static void AddServices(IServiceCollection serviceCollection, IConfiguration config)
+    {
+        serviceCollection.AddSingleton<HabboEncryption>();
+        serviceCollection.Configure<EncryptionOptions>(options => config.GetSection("Encryption").Bind(options));
+    }
+}
